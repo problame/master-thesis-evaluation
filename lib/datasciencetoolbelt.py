4d18c5bf06f1eb7d97feb3266b2fdb0053223043
Christian Schwarz <me@cschwarz.com>
2021-03-17 12:35:27 +0100

    add postprocessing ipython notebooks (almost the same as for intermediate presentation but adjusted to use ResultStorage)

---
+++ lib/datasciencetoolbelt.py
@@ -0,0 +1,49 @@
+from schema import Schema, Or
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+ConfigSchema = Schema({
+    "seaborn_context": Or("talk", "notebook", "paper", "poster"),
+    "savefig": {
+        "dir": Path,
+        "enable": bool,
+    }
+})
+
+_config = None
+
+def setup(config):
+    global _config
+    if _config is not None:
+        raise Exception("can only setup once")
+    _config = ConfigSchema.validate(config)
+
+    # configure seaborn
+    sns.set()
+    sns.set_style("whitegrid")
+    sns.set_palette(sns.color_palette('muted'))
+    sns.set_context(_config["seaborn_context"])
+    
+    if not _config["savefig"]["dir"].is_dir():
+        raise Exception(f"savefig.dir={config['savefig']['dir']} must be a directory")
+    
+
+def savefig(name: str):
+    global _config
+    
+    plt.tight_layout()
+    if not _config["savefig"]["enable"]:
+        return
+
+    formats = [
+        ("png", 100, ".100dpi.png"),
+        ("png", 200, ".200dpi.png"),
+        ("png", 300, ".300dpi.png"),
+        ("png", 400, ".400dpi.png"),
+        ("pdf", 300, ".300dpi.pdf")
+    ]
+    for format, dpi, suffix in formats:
+        p = (_config["savefig"]["dir"] / name).with_suffix(suffix)
+        plt.savefig(p, format=format, dpi=dpi)
